53790763c0a061893362011c61dfe855fb025165
shanole <73411129+shanole@users.noreply.github.com>
2021-09-04 22:08:41 -0700

    add constants for action types

--- src/reducers/posts-reducer.js
+++ src/reducers/posts-reducer.js
@@ -1,18 +1,20 @@
+import * as c from './../actions/ActionTypes';
+
 export default (state = {}, action) => {
     const { name, post, timeOpen, formattedWaitTime, id, vote, score} = action;
     switch (action.type) {
-        case 'ADD_POST':
+        case c.ADD_POST:
             return ({...state, [id]: {name, post, id, timeOpen, formattedWaitTime, score}
             });
-        case 'DELETE_POST':
+        case c.DELETE_POST:
           let newState = {...state};
           delete newState[id];
           return newState;
-        case 'UPDATE_TIME':
+        case c.UPDATE_TIME:
             const newPost = {...state[id], formattedWaitTime};
             const updatedState = {...state, [id]: newPost};
             return updatedState;
-        case 'UPDATE_SCORE':
+        case c.UPDATE_SCORE:
             const currScore = state[id].score;
             const newPost2 = {...state[id], score: currScore + vote};
             const updatedState2 = {...state, [id]: newPost2}
