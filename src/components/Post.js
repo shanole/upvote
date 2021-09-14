f14e7f53efeb41327a9c58c6afa160f7ea04c4a4
Karlson Drendel <kdrendel99@gmail.com>
2021-09-13 21:39:39 -0700

    Fix upvote/downvote functionality to only allow the user to vote only once, or to undo said vote. Add button CSS to indicate selected vote.

--- src/components/Post.js
+++ src/components/Post.js
@@ -6,28 +6,29 @@ function Post(props) {
   const firestore = useFirestore();
   const [disableUp, setDisableUp] = React.useState(false);
   const [disableDown, setDisableDown] = React.useState(false);
-  const [userVoted, setUserVoted] = React.useState(false);
-  const [prevVote, setPrevVote] = React.useState( 0);
+  const [voted, setVoted] = React.useState(false);
+  const [prevVote, setPrevVote] = React.useState(null);
+  const [upVoteColor, setUpVoteTrueColor] = React.useState('grey');
+  const [downVoteColor, setDownVoteTrueColor] = React.useState('grey');
+
   let editedText = null;
   if (props.edited) {
     editedText = "Edited";
   }
-
+  
   function whenVoteClicked(id, currScore, vote){
-    if (userVoted){
-        if (prevVote === 1){
-          const firestorePostScore = {
-            score: currScore - 1
-          }
-          firestore.update({collection: 'posts', doc: id}, firestorePostScore);
-        }
-        else {
-          const firestorePostScore = {
-            score: currScore + 1
-          }
-          firestore.update({collection: 'posts', doc: id}, firestorePostScore);
-        }
+    if (voted){
+      const firestorePostScore = {
+        score: currScore - prevVote
+      }
+      firestore.update({collection: 'posts', doc: id}, firestorePostScore);
+      setVoted(false);
+      setDisableDown(false);
+      setDisableUp(false);
+      setUpVoteTrueColor('grey');
+      setDownVoteTrueColor('grey');
     }
+  
     else {
       const firestorePostScore = {
         score: currScore + vote
@@ -35,7 +36,9 @@ function Post(props) {
       firestore.update({collection: 'posts', doc: id}, firestorePostScore);
       //toggles opposite button clicked to false
       vote === 1 ? setDisableDown(true) : setDisableUp(true);
-      setUserVoted(true);
+      vote === 1 ? setUpVoteTrueColor('green') : setUpVoteTrueColor('grey');
+      vote === -1 ? setDownVoteTrueColor('green') : setDownVoteTrueColor('grey');
+      setVoted(true);
       setPrevVote(vote);
     }
   }
@@ -51,8 +54,8 @@ function Post(props) {
         <p className="edited">{editedText}</p>
       </div>
       <div className="vote-buttons">
-        <button disabled={disableUp} onClick = {() => whenVoteClicked(props.id, props.score, 1)}>Upvote</button>
-        <button disabled={disableDown} onClick = {() => whenVoteClicked(props.id, props.score, -1)}>Downvote</button>
+        <button disabled={disableUp} style={{background:upVoteColor}} onClick = {() => whenVoteClicked(props.id, props.score, 1)}>Upvote</button>
+        <button disabled={disableDown} style={{background:downVoteColor}} onClick = {() => whenVoteClicked(props.id, props.score, -1)}>Downvote</button>
       </div>
     </React.Fragment>
     );
@@ -80,12 +83,16 @@ export default Post;
 //each time you click the button, it does the opposite
 
 // const [voted, setVoted] = React.useState(false);
-// const [voteUp, setVoteUp] = React.useState(null);
+// const [prevVote, setPrevVote] = React.useState(null);
 
 
 // function whenVoteClicked(id, currScore, vote){
 //   if (voted){
-
+//     const firestorePostScore = {
+//       score: currScore - prevVote
+//     }
+//     firestore.update({collection: 'posts', doc: id}, firestorePostScore);
+//     setVoted(false);
 //   }
 
 //   else {
@@ -97,12 +104,5 @@ export default Post;
 //     vote === 1 ? setDisableDown(true) : setDisableUp(true);
 //     setUserVoted(true);
 //     setPrevVote(vote);
-//     if (vote === 1){
-//        setVoteUp(true);
-//     }
-//    if (vote === -1){
-//      setVoteUp(false);
-//      }
-//     setVoted(!voted);
 //   }
 // }
