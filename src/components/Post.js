9d53b5e2f19e207a0e46dbf884d10b439fee91b3
shanole <73411129+shanole@users.noreply.github.com>
2021-09-14 18:58:07 -0700

    moved vote buttons to own component

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,63 +1,25 @@
 import React from 'react';
 import PropTypes from "prop-types";
-import { useFirestore } from 'react-redux-firebase';
+import VoteButtons from './VoteButtons';
 
 function Post(props) {
-  const firestore = useFirestore();
-  
-  const [disableUp, setDisableUp] = React.useState(false);
-  const [disableDown, setDisableDown] = React.useState(false);
-  const [voted, setVoted] = React.useState(false);
-  const [prevVote, setPrevVote] = React.useState(null);
-  const [upVoteColor, setUpVoteTrueColor] = React.useState('grey');
-  const [downVoteColor, setDownVoteTrueColor] = React.useState('grey');
 
   let editedText = null;
   if (props.edited) {
     editedText = "Edited";
   }
-  
-  function whenVoteClicked(id, currScore, vote){
-    if (voted){
-      const firestorePostScore = {
-        score: currScore - prevVote
-      }
-      firestore.update({collection: 'posts', doc: id}, firestorePostScore);
-      setVoted(false);
-      setDisableDown(false);
-      setDisableUp(false);
-      setUpVoteTrueColor('grey');
-      setDownVoteTrueColor('grey');
-    }
-  
-    else {
-      const firestorePostScore = {
-        score: currScore + vote
-      }
-      firestore.update({collection: 'posts', doc: id}, firestorePostScore);
-      //toggles opposite button clicked to false
-      vote === 1 ? setDisableDown(true) : setDisableUp(true);
-      vote === 1 ? setUpVoteTrueColor('green') : setUpVoteTrueColor('grey');
-      vote === -1 ? setDownVoteTrueColor('green') : setDownVoteTrueColor('grey');
-      setVoted(true);
-      setPrevVote(vote);
-    }
-  }
 
   return (
     <React.Fragment>
       <hr />
-      <div onClick= { () => props.whenPostClicked(props.id)}>
+      <div className="post" onClick= { () => props.whenPostClicked(props.id)}>
         <h3 className="post-content">{props.post}</h3>
         <p className="post-author">{props.name}</p>
         <p className="timestamp">{props.timePosted}</p>
         <p className="post-score">{props.score}</p>
         <p className="edited">{editedText}</p>
       </div>
-      <div className="vote-buttons">
-        <button disabled={disableUp} style={{background:upVoteColor}} onClick = {() => whenVoteClicked(props.id, props.score, 1)}>Upvote</button>
-        <button disabled={disableDown} style={{background:downVoteColor}} onClick = {() => whenVoteClicked(props.id, props.score, -1)}>Downvote</button>
-      </div>
+      <VoteButtons id={props.id} score={props.score}  />
     </React.Fragment>
     );
 }
@@ -72,38 +34,3 @@ Post.propTypes = {
 }
 
 export default Post;
-
-
-
-// -if you click a button, the other one should be disabled
-// -if you click the original button again, it removes the previous value
-
-//the post should know if you've clicked it before
-//if you click the same button again, it should REMOVE the previous vote
-
-//each time you click the button, it does the opposite
-
-// const [voted, setVoted] = React.useState(false);
-// const [prevVote, setPrevVote] = React.useState(null);
-
-
-// function whenVoteClicked(id, currScore, vote){
-//   if (voted){
-//     const firestorePostScore = {
-//       score: currScore - prevVote
-//     }
-//     firestore.update({collection: 'posts', doc: id}, firestorePostScore);
-//     setVoted(false);
-//   }
-
-//   else {
-//     const firestorePostScore = {
-//       score: currScore + vote
-//     }
-//     firestore.update({collection: 'posts', doc: id}, firestorePostScore);
-//     //toggles opposite button clicked to false
-//     vote === 1 ? setDisableDown(true) : setDisableUp(true);
-//     setUserVoted(true);
-//     setPrevVote(vote);
-//   }
-// }
---
+++ src/components/VoteButtons.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import { useFirestore } from 'react-redux-firebase';
+
+
+function VoteButtons(props) {
+  const firestore = useFirestore();
+  
+  const [disableUp, setDisableUp] = React.useState(false);
+  const [disableDown, setDisableDown] = React.useState(false);
+  const [voted, setVoted] = React.useState(false);
+  const [prevVote, setPrevVote] = React.useState(null);
+  const [upVoteColor, setUpVoteTrueColor] = React.useState('grey');
+  const [downVoteColor, setDownVoteTrueColor] = React.useState('grey');
+
+
+  function whenVoteClicked(id, currScore, vote){
+    if (voted){
+      const firestorePostScore = {
+        score: currScore - prevVote
+      }
+      firestore.update({collection: 'posts', doc: id}, firestorePostScore);
+      setVoted(false);
+      setDisableDown(false);
+      setDisableUp(false);
+      setUpVoteTrueColor('grey');
+      setDownVoteTrueColor('grey');
+    }
+  
+    else {
+      const firestorePostScore = {
+        score: currScore + vote
+      }
+      firestore.update({collection: 'posts', doc: id}, firestorePostScore);
+      //toggles opposite button clicked to false
+      vote === 1 ? setDisableDown(true) : setDisableUp(true);
+      vote === 1 ? setUpVoteTrueColor('green') : setUpVoteTrueColor('grey');
+      vote === -1 ? setDownVoteTrueColor('#2596be') : setDownVoteTrueColor('grey');
+      setVoted(true);
+      setPrevVote(vote);
+    }
+  }
+  
+  return (
+  <div className="vote-buttons">
+    <button disabled={disableUp} style={{background:upVoteColor}} onClick = {() => whenVoteClicked(props.id, props.score, 1)}>Upvote</button>
+    <button disabled={disableDown} style={{background:downVoteColor}} onClick = {() => whenVoteClicked(props.id, props.score, -1)}>Downvote</button>
+  </div>
+  );
+}
+
+export default VoteButtons;
