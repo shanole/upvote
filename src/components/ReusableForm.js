3709d9c940fe7f51e98743a33e991c36149be5f3
shanole <73411129+shanole@users.noreply.github.com>
2021-09-19 13:10:02 -0700

    fix form tags

--- src/components/ReusableForm.js
+++ src/components/ReusableForm.js
@@ -2,7 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
 
-const StyledForm = styled.form`
+const StyledForm = styled.div`
   input, textarea {
     display: block;
     width: 100%;
